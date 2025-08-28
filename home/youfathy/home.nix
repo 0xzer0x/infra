f65bea198afd8861994861cf85162e7b94811c36
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-08-28 10:31:12 +0300

    fix(hm): remove duplicate packages

--- home/youfathy/home.nix
+++ home/youfathy/home.nix
@@ -14,9 +14,6 @@
   # release notes.
   home.stateVersion = "24.05";
 
-  # The home.packages option allows you to install Nix packages into your environment.
-  home.packages = with pkgs; [ kitty tmux sesh neovim ];
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
