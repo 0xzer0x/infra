36ac43a5c89f415738ce87eab2281942484343a3
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-09-01 09:07:50 +0300

    fix(qt): disable wayland window decorations

--- home/features/desktop/hyprland/default.nix
+++ home/features/desktop/hyprland/default.nix
@@ -67,6 +67,7 @@ in {
           "HYPRCURSOR_SIZE,22"
           "NIXOS_OZONE_WL,1"
           "ELECTRON_OZONE_PLATFORM_HINT,wayland"
+          "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
         ];
 
         # NOTE: Look and feel
