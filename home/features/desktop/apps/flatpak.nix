4864d6750452da20756668a6a4b324c3227e5997
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-09-10 08:18:12 +0300

    chore(desktop/apps): enable weekly updates of flatpaks

--- home/features/desktop/apps/flatpak.nix
+++ home/features/desktop/apps/flatpak.nix
@@ -6,13 +6,20 @@ in {
   imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
 
   config = mkIf cfg.enable {
-    services.flatpak.packages = [
-      "app.zen_browser.zen"
-      "org.telegram.desktop"
-      "com.github.tchx84.Flatseal"
-      "org.gtk.Gtk3theme.adw-gtk3"
-      "org.gtk.Gtk3theme.adw-gtk3-dark"
-      "io.github._0xzer0x.qurancompanion"
-    ];
+    services.flatpak = {
+      packages = [
+        "app.zen_browser.zen"
+        "org.telegram.desktop"
+        "com.github.tchx84.Flatseal"
+        "org.gtk.Gtk3theme.adw-gtk3"
+        "org.gtk.Gtk3theme.adw-gtk3-dark"
+        "io.github._0xzer0x.qurancompanion"
+      ];
+
+      update.auto = {
+        enable = true;
+        onCalendar = "weekly";
+      };
+    };
   };
 }
