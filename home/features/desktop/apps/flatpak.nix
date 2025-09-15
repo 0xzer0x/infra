53f54d10a409b9b8cd7e871d0818e5511cd3145c
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-09-15 10:53:36 +0300

    feat(apps/flatpak): add flatpak overrides

--- home/features/desktop/apps/flatpak.nix
+++ home/features/desktop/apps/flatpak.nix
@@ -16,6 +16,34 @@ in {
         "io.github._0xzer0x.qurancompanion"
       ];
 
+      overrides = {
+        global = {
+          Context = {
+            sockets = [ "wayland" "fallback-x11" "!x11" ];
+            devices = [ "dri" ];
+            filesystems = [
+              # NOTE: Makes system fonts accessible in Flatpaks
+              # See https://nixos.wiki/wiki/Fonts
+              "/nix/store:ro"
+              "xdg-data/fonts:ro"
+              "xdg-config/fontconfig:ro"
+              # NOTE: Make Flatpaks follow system Gtk theme
+              "xdg-config/gtk-3.0:ro"
+              "xdg-config/gtk-4.0:ro"
+            ];
+          };
+
+          Environment = {
+            QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+            QT_QPA_PLATFORMTHEME = "qt5ct";
+            QT_QPA_PLATFORM = "wayland;xcb";
+            ELECTRON_OZONE_PLATFORM_HINT = "auto";
+            MOZ_DISABLE_RDD_SANDBOX = "1";
+            MOZ_ENABLE_WAYLAND = "1";
+          };
+        };
+      };
+
       update.auto = {
         enable = true;
         onCalendar = "weekly";
