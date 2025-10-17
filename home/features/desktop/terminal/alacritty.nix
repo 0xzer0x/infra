1a4fcdb9a3d067d1bfa35cc22986f5ee9e492af5
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-10-17 09:51:52 +0300

    feat(desktop/terminal): add optional alacritty configuration

---
+++ home/features/desktop/terminal/alacritty.nix
@@ -0,0 +1,22 @@
+{ config, lib, ... }:
+
+with lib;
+let cfg = config.features.desktop.terminal;
+in {
+  options.features.desktop.terminal.alacritty.enable = mkOption {
+    type = types.bool;
+    description = "Enable Alacritty terminal configuration";
+    default = cfg.default == "alacritty";
+  };
+
+  config = mkIf cfg.alacritty.enable {
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        terminal = { shell = "tmux"; };
+        font = { size = 11; };
+      };
+    };
+  };
+}
+
--- home/features/desktop/terminal/default.nix
+++ home/features/desktop/terminal/default.nix
@@ -4,10 +4,10 @@ with lib;
 let cfg = config.features.desktop.terminal;
 
 in {
-  imports = [ ./kitty.nix ./foot.nix ];
+  imports = [ ./kitty.nix ./foot.nix ./alacritty.nix ];
 
   options.features.desktop.terminal.default = mkOption {
-    type = types.enum [ "kitty" "foot" ];
+    type = types.enum [ "kitty" "foot" "alacritty" ];
     description = "Name of default terminal program";
     default = "kitty";
   };
