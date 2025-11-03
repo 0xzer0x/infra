fb57cb92ca3b470e15e80f132e41c46794e4ce1a
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-11-03 09:32:05 +0200

    fix(cli/ansible): collections not detected by ansiblels

--- home/features/cli/ansible.nix
+++ home/features/cli/ansible.nix
@@ -7,12 +7,22 @@ in {
     mkEnableOption "Enable Ansible CLI utilities";
 
   config = mkIf cfg.enable {
-    # NOTE: Additional packages
-    home.packages = with pkgs; [ ansible ];
+    # FIX: Updated Ansible package dependencies to contain boto3 for AWS collection modules
+    # Nixpkg: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/development/python-modules/ansible/core.nix
+    # Ref: https://nixos.org/manual/nixpkgs/stable/#buildpythonpackage-function
+    home.packages = let
+      ansible = (pkgs.ansible.overridePythonAttrs (finalAttrs: {
+        dependencies = finalAttrs.dependencies
+          ++ (with pkgs.python3Packages; [ boto3 ]);
+      }));
+    in [ ansible ];
 
     # NOTE: Extra environment variables
     home.sessionVariables = {
       ANISBLE_HOME = "${config.xdg.configHome}/ansible";
+      # WARN: Required for ansible-language-server
+      ANSIBLE_COLLECTIONS_PATH =
+        "${pkgs.python3Packages.ansible}/lib/python${pkgs.python3.pythonVersion}/site-packages";
     };
   };
 }
