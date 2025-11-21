3e767609e606f6f4490f039450b4f55bf06478a1
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-11-21 13:12:28 +0200

    fix(desktop/hyprland): missing key

--- home/features/desktop/hyprland/default.nix
+++ home/features/desktop/hyprland/default.nix
@@ -102,7 +102,6 @@ in
         misc = {
           disable_hyprland_logo = true;
           disable_splash_rendering = true;
-          disable_hyprland_qtutils_check = true;
         };
 
         # NOTE: Environment variables (imported session variables and add additional ones)
