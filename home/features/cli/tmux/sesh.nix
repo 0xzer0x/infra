7ee5af3118bba623920ec7cc7653d6116dc439cf
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-11-23 09:24:51 +0200

    fix(cli/tmux): use editor variable from session vars attrset

--- home/features/cli/tmux/sesh.nix
+++ home/features/cli/tmux/sesh.nix
@@ -11,7 +11,7 @@ let
         { name, path }:
         {
           inherit name path;
-          startup_command = "\${EDITOR}";
+          startup_command = config.home.sessionVariables.EDITOR;
         }
       )
       [
