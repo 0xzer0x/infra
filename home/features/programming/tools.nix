9584153d7b504458e1a82a7ef9959cfccd74fee8
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2026-03-27 14:14:17 +0200

    refactor(features/programming): remove devbox and add posting for api testing

--- home/features/programming/tools.nix
+++ home/features/programming/tools.nix
@@ -8,14 +8,21 @@
 with lib;
 let
   cfg = config.features.programming.tools;
+  yaml = pkgs.formats.yaml { };
 in
 {
   options.features.programming.tools.enable = mkEnableOption "Enable development environment tools";
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      devbox
       devenv
+      posting
     ];
+
+    xdg.configFile."posting/config.yaml" = {
+      source = yaml.generate "posting-config.yaml" {
+        theme = "catppuccin-mocha";
+      };
+    };
   };
 }
